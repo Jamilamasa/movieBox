ef118c9788ce5dacd6b5631d9dc39eafd5b08655
JamilAmasa <jamilamasa@gmail.com>
2023-09-13 01:36:45 +0100

    fix(card): add test-data-id

--- src/Components/FeaturedMovies/Card.jsx
+++ src/Components/FeaturedMovies/Card.jsx
@@ -6,19 +6,29 @@ import Rating from "../UI/Rating";
 const Card = ({ imagePath, title, releaseDate, rating, genre }) => {
   const genreNames = useGenreNames(genre);
 
-
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col" data-testid="movie-card">
       {/* Poster */}
       <div>
-        <img src={getMovieUrl(imagePath)} alt={title} />
+        <img
+          src={getMovieUrl(imagePath)}
+          alt={title}
+          data-testid="movie-poster"
+        />
       </div>
       {/* Info */}
       <div className="flex flex-col">
         {/* Release Date */}
-        <p className="text-[#9CA3AF] text-xs font-bold mt-4">{releaseDate}</p>
+        <p
+          className="text-[#9CA3AF] text-xs font-bold mt-4"
+          data-testid="movie-release-date"
+        >
+          {releaseDate}
+        </p>
         {/* Title */}
-        <h1 className="font-bold text-lg mt-5">{title}</h1>
+        <h1 className="font-bold text-lg mt-5" data-testid="movie-title">
+          {title}
+        </h1>
         {/* Rating */}
         <Rating imdbRating={rating} tomatoRating="50" />
         {/* Genre */}
